4b09d094a638ca9cdb3de9c94263cd6e8617f266
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-10-22 13:19:32 -0400

    controls a servo

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
+ * Created by: Sultan
  * Created on: Sep 2020
- * This program ...
+ * This program controls a servo
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// when A is pressed turn to 0
+input.onButtonPressed(Button.A, function() { 
+    basic.clearScreen()
+    basic.showString("0")
+    robotbit.Servo(servoNumber1, 0)
+    basic.showIcon(IconNames.Happy)
+    })
+
+// when B is pressed turn to 180
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("180")
+    robotbit.Servo(servoNumber1, 180)
+    basic.showIcon(IconNames.Happy)
+})
+
